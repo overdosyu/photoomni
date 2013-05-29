4dd94708195bcfc9ef6b01da21de6d70f7c9b582
Simon Yu <syu@Simon-Yus-MacBook-Pro.local>
2013-05-29 12:58:20 -0700

    Customizing Devise reset password pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130528195010) do
+ActiveRecord::Schema.define(:version => 20130528214813) do
 
   create_table "post_likes", :force => true do |t|
     t.integer  "post_id"
@@ -76,17 +76,18 @@ ActiveRecord::Schema.define(:version => 20130528195010) do
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "encrypted_password"
-    t.datetime "created_at",                          :null => false
-    t.datetime "updated_at",                          :null => false
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",        :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "failed_attempts",      :default => 0
+    t.integer  "failed_attempts",        :default => 0
+    t.datetime "reset_password_sent_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
