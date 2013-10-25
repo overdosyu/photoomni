a04b3ea708b1928804a8171818bb4f88ae585866
overdosyu <overdosyu@gmail.com>
2013-10-24 17:14:26 -0700

    posts in topic order by id

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ActiveRecord::Base
   include Rails.application.routes.url_helpers
 
   belongs_to :user
-  has_many :posts
+  has_many :posts, :order => :id
   attr_accessible :title
 
   def url
